ee518d85876c7cf0b4a622e693639b45ac8e9df7
hassanio <20100056@lums.edu.pk>
2018-07-09 18:57:58 +0500

    Done

--- index.android.js
+++ index.android.js
@@ -3,8 +3,4 @@
 import { NativeModules } from 'react-native'
 // name as defined via ReactContextBaseJavaModule's getName
 
-@Override
-public String getName() {
-    return "Reactshit";
-}
 module.exports = NativeModules.Reactshit
